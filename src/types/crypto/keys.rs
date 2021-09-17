eaa96475f7a84083f9428661c358c5d9677ae0de
Joera <joeramulders@gmail.com>
2021-09-17 18:45:21 +0200

    amehoela

--- src/types/crypto/keys.rs
+++ src/types/crypto/keys.rs
@@ -152,7 +152,7 @@ impl PrivateKey {
             v: signature.v.into(),
             r: U256::from_big_endian(signature.r.as_bytes()),
             s: U256::from_big_endian(signature.s.as_bytes()),
-            chain_id: "01".into()
+            chain_id: 1.into()
         })
     }
 
